3962b3bf5d0461f8b0ce3033833c0975cd30a110
chayeoi <chayeoikeem@gmail.com>
2019-10-19 17:31:11 +0900

    chore: 초기 전역 상태 분리

--- src/components/GlobalProvider.js
+++ src/components/GlobalProvider.js
@@ -2,10 +2,9 @@ import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
 
 import { DispatchContext, GlobalContext } from '../contexts';
-import { reducer } from '../store';
+import { initialState, reducer } from '../store';
 
 const GlobalProvider = ({ children }) => {
-  const initialState = {};
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,36 @@
+export const initialState = {
+  user: null,
+  repos: null,
+  loading: false,
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'FETCH_USER_REQUEST':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'FETCH_USER_SUCCESS':
+      return {
+        ...state,
+        user: action.payload.user,
+      };
+    case 'INIT_USER':
+      return {
+        ...state,
+        user: null,
+      };
+    case 'FETCH_REPOS_BY_USER_REQUEST':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'FETCH_REPOS_BY_USER_SUCCESS':
+      return {
+        ...state,
+        repos: action.payload.repos,
+      };
     default:
       return state;
   }
