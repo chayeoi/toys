d2c7348c17ddf7353d32c29edb01d78a79acfae6
chayeoi <chayeoikeem@gmail.com>
2019-10-20 16:00:50 +0900

    chore: 데이터 로딩 중일 경우 인디케이터 표시

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { get } from 'lodash/fp';
 
-import { Repos, UserInfo } from '../components';
+import {
+  ErrorMessage, Loader, Repos, UserInfo,
+} from '../components';
 import { useDispatch, useSelector } from '../hooks';
 import * as services from '../services';
 
 const User = () => {
   const user = useSelector((state) => state.user);
   const repos = useSelector((state) => state.repos);
+  const loading = useSelector((state) => state.loading);
+  const error = useSelector((state) => state.error);
 
   const dispatch = useDispatch();
 
   const params = useParams();
 
-  console.log('user:', user);
-  console.log('repos:', repos);
-
   useEffect(() => {
-    try {
-      const fetchUser = async () => {
+    const fetchUser = async () => {
+      try {
         dispatch({ type: 'FETCH_USER_REQUEST' });
 
         const responses = await Promise.all([
@@ -31,22 +33,33 @@ const User = () => {
           type: 'FETCH_USER_SUCCESS',
           payload: { user: responses[0].data, repos: responses[1].data },
         });
-      };
+      } catch (e) {
+        dispatch({
+          type: 'FETCH_USER_FAILURE',
+          payload: { error: e },
+        });
+      }
+    };
 
-      fetchUser();
-    } catch (error) {
-      dispatch({
-        type: 'FETCH_USER_FAILURE',
-        payload: { error },
-      });
-    }
+    fetchUser();
   }, [dispatch, params.user]);
 
+  if (get('response.status', error) === 403) {
+    return (
+      <ErrorMessage>
+        {error.response.data.message}
+      </ErrorMessage>
+    );
+  }
+
   return (
-    <div>
-      {user && <UserInfo user={user} />}
-      {repos && <Repos repos={repos} />}
-    </div>
+    <>
+      <div>
+        {user && <UserInfo user={user} repos={repos} />}
+        {repos && <Repos repos={repos} />}
+      </div>
+      {loading && <Loader />}
+    </>
   );
 };
 
