a0f26a2cbe8d8e426322dc47424ef5f212df744c
chayeoi <chayeoikeem@gmail.com>
2020-10-13 23:52:14 +0900

    feat: `photo-gallery` 추가
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ photo-gallery/README.md
@@ -0,0 +1,8 @@
+# Photo Gallery
+
+- Using custom firebase hooks.
+- Using Framer motion.
+
+## Resources
+
+- [Build a Photo Gallery With React & Firebase | Traversy Media - YouTube](https://youtu.be/vUe91uOx7R0)
---
+++ photo-gallery/src/hooks/useFirestore.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { firestore } from '../firebase/config';
+
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firestore.collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(snap => {
+        const documents = [];
+
+        snap.forEach(doc => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+
+        setDocs(documents);
+      });
+
+    return unsubscribe;
+  }, [collection]);
+
+  return { docs };
+}
+
+export default useFirestore;
